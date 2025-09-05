7da4182eb892ead12ce06955631d32f96105faa9
muhammadhasan <muhammadhasanmurodov>
2025-09-05 07:52:27 +0500

    added commit

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -4,7 +4,7 @@ import { db } from "../firebase/config";
 
 export const useCollection = (collectionName) => {
   const [data, setData] = useState(null);
-
+  
   useEffect(() => {
     const unsubscribe = onSnapshot(
       collection(db, collectionName),
