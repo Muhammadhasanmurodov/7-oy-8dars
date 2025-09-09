1e9c1772ad43789994b82f6d543358adfadd2fcd
muhammadhasan <muhammadhasanmurodov>
2025-09-10 04:48:05 +0500

    addedd commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCr0tRmmp-3HFGwWau8RuE52--BR12i2iM",
+  authDomain: "muhammadhasan-project.firebaseapp.com",
+  projectId: "muhammadhasan-project",
+  storageBucket: "muhammadhasan-project.firebasestorage.app",
+  messagingSenderId: "631710030691",
+  appId: "1:631710030691:web:70fcc845322a2b9e95d156"
 };
 
 // Initialize Firebase
